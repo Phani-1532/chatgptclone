3083c2be641045a86d95e4dd09f5d9fffcfaa47e
Phanindra <phanindragadireddy2@gmail.com>
2025-01-24 16:42:20 +0530

    upperSide part completed

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import gptLogo from './assets/chatgpt.svg'
+import addBtn from './assets/add-30.png'
+import msgIcon from './assets/message.svg'
+import home from './assets/home.svg'
+import rocket from './assets/rocket.svg'
+import saved from './assets/bookmark.svg'
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className='sideBar'>
+        <div className='upperSide'>
+          <div className='upperSideTop'>
+            <img src={gptLogo} alt='chatGpt logo' className='logo' /> <span className='brand'>ChatGPT</span>
+          </div>
+          <button className='midBtn'><img src={addBtn} className='addBtn' alt='addBtn' />New Chat</button>
+          <div className='upperSideBottom'>
+              <button className='query'><img src={msgIcon} alt='first Btn' />What is Programming?</button>
+              <button className='query'><img src={msgIcon} alt='second Btn' />How to use an AI?</button>
+          </div>
+        </div>
+        <div className='lowerSide'>
+          <div className='listItems'><img src={home} alt='' className='listitemsImg' />Home</div>
+          <div className='listItems'><img src={saved} alt='' className='listitemsImg' />Saved</div>
+          <div className='listItems'><img src={rocket} alt='' className='listitemsImg' />Upgrade to Pro</div>
+        </div>
+      </div>
+      <div className='main'></div>
+      
+    </div>
+  );
+}
+
+export default App;
